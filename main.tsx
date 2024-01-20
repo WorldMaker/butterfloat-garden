33be185fd91d00a4229686922b4f80dff7cd1abc
Max Battcher <me@worldmaker.net>
2024-01-20 11:38:42 -0500

    Update to dynamic example

--- main.tsx
+++ main.tsx
@@ -1,15 +1,27 @@
 import { jsx, run } from 'butterfloat'
+import { Observable, concat, interval, of, map } from 'rxjs'
 
 interface HelloProps {
-  to: string
+  to: Observable<string>
 }
 
 function Hello({ to }: HelloProps) {
-  return <p className="hello">Hello {to}</p>
+  const innerText = to.pipe(map((to) => `Hello ${to}`))
+  return <p className="hello" bind={{ innerText }} />
 }
 
 function Main() {
-  return <Hello to="World" />
+  const greetable = ['World', 'Butterfloat', 'User']
+
+  // starting with "World" show a random greeting every 15 seconds
+  const helloTo = concat(
+    of('World'),
+    interval(15_000 /* ms */).pipe(
+      map(() => greetable[Math.round(Math.random() * greetable.length)]),
+    ),
+  )
+
+  return <Hello to={helloTo} />
 }
 
 const container = document.getElementById('container')!
